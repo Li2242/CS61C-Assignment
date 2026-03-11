85576b359133033d3306a5c0ab85ee6618660acb
lzy <2242751924@qq.com>
2026-03-11 16:43:55 +0800

    lab01有一个判断链表是否为环的内容非常有意思

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,45 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+	if(head == NULL){
+		return 0;
+	} 
+		
+	node *slow = head;
+	node *fast = head;
+	while(fast != NULL && fast->next != NULL){
+		slow = slow->next;
+		fast = fast->next->next;
+		if(slow == fast){
+			return 1;
+		}
+	}
     return 0;
 }
+
+// node* find_cycle_start(node* head){
+// 	node *meet = ll_has_cycle(head);
+// 	if(meet == NULL) return NULL;
+// 	node *p1 = head;
+// 	node *p2 = meet;
+// 	//p1 == p2时就是环的起点
+// 	while(p1 != p2){
+// 		p1 = p1->next;
+// 		p2 = p2->next;
+// 	}
+// 	return p1;
+// }
+
+// int cycle_length(node *head){
+// 	node* meet = ll_has_cycle(head);
+// 	if(meet == NULL) return 0;
+
+// 	int length = 1;
+// 	node *current = meet->next;
+// 	//从相遇点绕一圈再到相遇点
+// 	while(current != meet){
+// 		length++;
+// 		current = current->next;
+// 	}
+// 	return length;
+// }
